24bdea3fbd3810729bab8b13ba426e089ca05087
Vrutika <56282134+Vrutika0403@users.noreply.github.com>
2024-03-07 18:30:35 +1100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,6 +16,7 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 # Create a Car Model model
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
